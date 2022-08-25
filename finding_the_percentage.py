518676957fca952f7cf6d5b820a7fc04e5e5714e
Pranes <pranesracer@gmail.com>
2022-08-25 13:33:33 -0400

    checking committer details

--- finding_the_percentage.py
+++ finding_the_percentage.py
@@ -6,8 +6,5 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
-    
-
-    
     print('{:.2f}'.format(sum(student_marks[query_name])/len(student_marks[query_name])))
 
